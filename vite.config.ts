2c481aa2d25d4387f9d6abe29bcee966c6b05ab3
Chloechu12 <phucthocchu12@gmail.com>
2026-06-24 19:37:50 -0500

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
-import path from 'node:path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [
-    tailwindcss(),
-    react(),
-
-  ],
+  plugins: [tailwindcss(), react()],
   server: {
-    allowedHosts: ["mardi-opposition-archive-motors.trycloudflare.com " ],
+    allowedHosts: true,
   },
-})
+});
